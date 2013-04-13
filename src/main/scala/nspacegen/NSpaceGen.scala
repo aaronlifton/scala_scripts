ff437ba57a187d07a2534f2951bd2bd6ac5ed87b
Aaron Lifton <theflubba@gmail.com>
2013-04-13 16:31:03 -0400

    commit

--- src/main/scala/nspacegen/NSpaceGen.scala
+++ src/main/scala/nspacegen/NSpaceGen.scala
@@ -1,49 +1,23 @@
 package main.scala.nspacegen
 
 import scala.util.Random
-import scala.collection.mutable.ArrayBuffer
 
 object NSpaceGen extends App {
-  def n_rands(n: Int, r: Int) = {
-    Seq.fill(n)(Random.nextInt)
+  def random2dArray(dim1: Int, dim2: Int, max: Int): Array[Array[Int]] = {
+    Array.fill(dim1, dim2)(Random.nextInt(max))
   }
 
-  var rands = n_rands(10, 3)
-
-  def randomArray(n: Int): Array[Int] = {
-    val range = 0 to 100
-    var a: Array[Int] = new Array[Int](n)
-
-    def getRndInt(range: Range): Int = {
-      val rnd = new scala.util.Random
-      rnd.nextInt(range.length)
-    }
-
-    for (index <- 0 to a.length - 1) a(index) = (getRndInt(range))
-    a
+  type AA[B] = Array[Array[B]]
+  def compareArrays[A](a1: AA[A], a2: AA[A]): AA[A] = {
+   a1.filter(x => a2.exists(y => x.sameElements(y)))
   }
 
-  def randomArrays(n: Int, r: Int): ArrayBuffer[Array[Int]] = {
-    var a = ArrayBuffer[Array[Int]]()
-    for (i <- 0 to n) a += randomArray(r)
-    return a
-  }
+  val a1 = random2dArray(1000, 3, 100)
+  val a2 = random2dArray(1000000, 3, 100)
 
-  //var point = randomArray(3)
+  val matches = compareArrays[Int](a1, a2)
 
-  val thous = randomArrays(1000, 3)
-  val milli = randomArrays(10000, 3)
-
-  def compareArrays(mil: ArrayBuffer[Array[Int]], thous: ArrayBuffer[Array[Int]]): ArrayBuffer[Array[Int]] = {
-    var matched = ArrayBuffer[Array[Int]]()
-    mil.foreach { m =>
-      thous.foreach(t => if (m.deep == t.deep) {matched += t})
-    }
-    matched
-  }
-
-  val matches = compareArrays(milli, thous)
-  if (matches.length == 0) println("No matches found")
+  if (matches.isEmpty) println("No matches found")
   else {
     println(matches.map(_.mkString(" ")).mkString("\n"))
     println("\n" + matches.length + " matches found")
