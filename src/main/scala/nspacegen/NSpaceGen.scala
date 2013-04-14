6e1cf35183a9427e564071554eee40c62dda6819
Aaron Lifton <theflubba@gmail.com>
2013-04-13 21:39:13 -0400

    commit

--- src/main/scala/nspacegen/NSpaceGen.scala
+++ src/main/scala/nspacegen/NSpaceGen.scala
@@ -1,21 +1,33 @@
 package main.scala.nspacegen
 
-import scala.util.Random
+import scala.collection.mutable.ArrayBuffer
 
 object NSpaceGen extends App {
-  def random2dArray(dim1: Int, dim2: Int, max: Int): Array[Array[Int]] = {
-    Array.fill(dim1, dim2)(Random.nextInt(max))
-  }
 
   type AA[B] = Array[Array[B]]
   def compareArrays[A](a1: AA[A], a2: AA[A]): AA[A] = {
    a1.filter(x => a2.exists(y => x.sameElements(y)))
   }
 
-  val a1 = random2dArray(1000, 3, 100)
-  val a2 = random2dArray(1000000, 3, 100)
+  val points1 = ArrayBuffer[Array[Int]]()
+  val points2 = ArrayBuffer[Array[Int]]()
+  var curr = 0
 
-  val matches = compareArrays[Int](a1, a2)
+  Iterator.continually(Console.readLine).takeWhile(_ != "" && curr < 2).foreach {
+    line => {
+      try {
+        val s = line.split(" ").map(_.toInt)
+        curr match {
+          case 0 => points1 += s
+          case 1 => points2 += s
+        }
+      }
+      catch { case e => "Error: " + e.getMessage }
+      if (line == ".") curr += 1
+    }
+  }
+
+  val matches = compareArrays[Int](points1.toArray, points2.toArray)
 
   if (matches.isEmpty) println("No matches found")
   else {
--- src/test/scala/NSpaceGenSuite.scala
+++ src/test/scala/NSpaceGenSuite.scala
@@ -1,22 +1,48 @@
 package test.scala
 
 import org.scalatest.FunSuite
-import main.scala.nspacegen.NSpaceGen
+import main.scala.nspacegen.NSpaceGen.{compareArrays, AA}
+import scala.util.Random
 
 class NSpaceGenSuite extends FunSuite {
 
-  val ta: Array[Array[Int]] = NSpaceGen.random2dArray(1000, 3, 100)
+  def random2dArray(dim1: Int, dim2: Int, max: Int): AA[Int] = {
+    Array.fill(dim1, dim2)(Random.nextInt(max))
+  }
+
+  val a1 = random2dArray(1000, 3, 100)
+  val a2 = random2dArray(1000000, 3, 100)
 
   test("arrays generated with correct sizes") {
-    assert(ta.length == 1000)
-    assert(ta(0).length == 3)
+    assert(a1.length == 1000)
+    assert(a1(0).length == 3)
   }
 
   test("arrays have correct values") {
-    val tb: Array[Array[Int]] = NSpaceGen.random2dArray(1000, 3, 100)
+    val tb = random2dArray(1000, 3, 100)
     val min = tb.map(_.min).min
     val max = tb.map(_.max).max
     assert(min >= 0)
     assert(max <= 100)
+  }
+
+  test("arrays with same first element find it as a match") {
+    val tb = random2dArray(1000, 3, 100)
+    val td = random2dArray(1000, 3, 100)
+    val u = Array(1,2,3)
+    val matches = compareArrays[Int](tb.updated(0, u), td.updated(0, u))
+    assert(matches.exists(_.deep == u.deep))
+  }
+
+  test("two arrays with unique elements find no matches") {
+    val tb = Array(Array(1,2,3), Array(2,3,4))
+    val td = Array(Array(3,4,5), Array(6,7,2))
+    val matches = compareArrays[Int](tb, td)
+    assert(matches.isEmpty)
+  }
+
+  test("arrays size 1000 and 1000000 have at least 1 match") {
+    val matches = compareArrays[Int](a1, a2)
+    assert(!matches.isEmpty)
   }
 }
