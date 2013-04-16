960d5b6e5c7eb00cd9e6b9ccf0e5d34da6f2af33
Aaron Lifton <theflubba@gmail.com>
2013-04-16 13:20:18 -0400

    commit

--- src/main/scala/drunkard/Drunkard.scala
+++ src/main/scala/drunkard/Drunkard.scala
@@ -2,13 +2,14 @@
 package main.scala.drunkard
 
 import scala.collection.mutable.ArrayBuffer
+import scala.math.{pow, sqrt, exp, Pi}
 
 object Drunkard extends App {
   // val input = readLine("prompt> ")
   var a: ArrayBuffer[Array[String]] = ArrayBuffer[Array[String]]()
   Iterator.continually(Console.readLine).takeWhile(_ != null).foreach(line => a += line.split(" "))
 
-  def walk(n: Int) = {
+  def pseudo(n: Int) = {
     val multiplier: Long = 0
     val modulus: Long = 0
     var seed: Int = 0
@@ -19,5 +20,40 @@ object Drunkard extends App {
 
   }
 
-  println("Done")
+  def walk(m: Int) = {
+
+  }
+  def histrogram(m: Int, n: Int, seed: Int) = {
+    return "a"
+  }
+
+ /* def N(p: Int, m: Int) =  {
+    return exp(pow(-1*p, 2)/(2*m)) / sqrt(2*Pi*m)
+  }
+
+
+  def H(p: Int) = {
+
+  }
+
+  def printWalk = {
+    println("-");
+  }
+}
+
+/*def reverse(s: String) = {
+  s.length match {
+    case 0
+  }
+  1
+  > 1  {
+      swap head and tail
+
+  }
+}
+
+// memorize Aho Hopcroft and ullman - Data Structures
+//
+*/
+* */
 }
